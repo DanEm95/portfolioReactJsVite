ea26846c5190477ac7931f207c070abb0ca168ae
DanEm95 <110663668+DanEm95@users.noreply.github.com>
2025-01-06 18:17:06 +0100

    updated bloqWithUsers image

--- src/App.tsx
+++ src/App.tsx
@@ -122,7 +122,7 @@ function App() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 py-10 space-x-5 space-y-5">
-            <img src="/Assets/passwordManager.png" className="rounded-2xl" />
+            <img src="/Assets/blogWithUsers.png" className="rounded-2xl" />
             <div className="text flex flex-col justify-center items-start gap-3">
               <div className="text-3xl font-bold">Blog With Users</div>
               <div className="text-xl text-gray-300">
